757270f9c28f0d300587772d537d9857c12280af
Saurab-Negi <saurabnegi17@gmail.com>
2024-08-30 16:48:53 +0530

    Socket connection established

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const http = require('http'); // Node.js built-in module for creating an HTTP server
+const { Server }= require('socket.io') // A class that allows creating a WebSocket server
+
+const server= http.createServer(app); // Creates an HTTP server using the Express app
+const io= new Server(server); // Socket.IO server that listens for WebSocket connections on the HTTP server
+const userSocketMap= {};
+
+const getAllConnectedClients = (roomId) => { // getting the roomId
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+        (socketId) =>{
+            return {
+                socketId,
+                username: userSocketMap[socketId],
+            }
+        }
+    );
+}
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`)
+    // Sets up an event listener that triggers whenever a new client connects to the Socket.IO server.
+    // socket: Represents the connected client with a unique id
+
+    socket.on('join', ({ roomId, username }) => {
+        userSocketMap[socket.id] = username;
+        socket.join(roomId);
+        const clients= getAllConnectedClients(roomId);
+        clients.forEach(({socketId}) =>{
+            // Notify other users that new user has joined
+            io.to(socketId).emit('joined', {
+                clients,
+                username,
+                socketId: socket.id,
+            })
+        })
+    })
+}) 
+
+const PORT= process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+});
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import logo from '../../assets/logo.png'
 import Client from '../Client/Client'
+import { initSocket } from '../../Socket'
+import { toast } from "react-hot-toast";
+import { Navigate, useLocation, useNavigate, useParams } from "react-router-dom"
 
 const Sidebar = () => {
 
-    const [client, setClient]= useState([
-        {socketId: 1,username: "Saurab"},
-        {socketId: 2,username: "Akshit"},
+  const socketRef= useRef(null);
+  const location = useLocation();
+  const { roomId }= useParams();
+  const navigate= useNavigate();
 
-    ])
+  const [client, setClient]= useState([]);
+
+  useEffect(() =>{
+    const init= async () =>{
+      socketRef.current= await initSocket();
+      socketRef.current.on('connect_error', (err) => handleError(err));
+      socketRef.current.on('connect_failed', (err) => handleError(err));
+
+      const handleError= (e) =>{
+        console.log('socket error: ',e);
+        toast.error("Socket connection failed");
+        navigate('/');
+      }
+      socketRef.current.emit('join', {
+        roomId,
+        username: location.state?.username,
+      })
+      socketRef.current.on('joined', ({clients, username, socketId}) =>{
+        if(username !== location.state?.username){
+          toast.success(`${username} joined`);        
+        }
+        setClient(clients);
+    })
+    }
+    init();
+  },[])
+
+    if(!location.state){
+      return <Navigate to='/' />;
+    }
 
   return (
     <div className='w-full max-w-xs h-screen bg-slate-800 text-white flex flex-col justify-start items-center gap-3 shadow-inner rounded-r-xl px-4 py-8'>
